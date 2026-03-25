0e94439c976781ab3cf3d95f9377b9dd0886617e
Prince <princemungra1340@gmail.com>
2026-03-25 14:52:09 +0530

    removed frontend (hms-app), keep only backend

--- hms-app/src/api/hospitalApi.js
+++
@@ -1,9 +0,0 @@
-import API from "./axios";
-
-export const addHospital = (hospitalData) => {
-  return API.post("/hospitals", hospitalData);
-};
-
-export const fetchHospitalsByCity = (city) => {
-  return API.get(`/hospitals?city=${city}`);
-};
